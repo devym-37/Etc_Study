62ae437ae99d0da037d8a65b30426e2643d7c2c0
miniyoung37 <miniyoung37@gmail.com>
2019-07-23 13:48:24 +0900

    momentum localStorage 수정

--- momentum_st2.js
+++ momentum_st2.js
@@ -1,41 +1,44 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+  SHOWING_CN = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}           // localStorage에 저장
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = input.value;
     paintGreeting(currentValue);
-}
-
+    saveName(currentValue);
+}           // 사용자 이름 저장 방법
 
 function askForName(){
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit");
+    form.addEventListener("submit", handleSubmit);
 }
 
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
-function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
-        askForName();
-        // she is not
-    } else {
-        paintGreeting(currentUser);
-        // she is
-    }
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+    // she is not
+  } else {
+    paintGreeting(currentUser);
+  }
 }
 
-
-function init(){
-
+function init() {
+  loadName();
 }
+
 init();
