d4a5a26e58fdda71d48e49053cb3bcdade4f1f5c
miniyoung37 <miniyoung37@gmail.com>
2019-07-24 17:22:28 +0900

    momentum weather data api 입/출력

--- momentum_weather.js
+++ momentum_weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "29aea8d3ac1ab59c44a6ab34b9f5742e";
 const COORDS = "coords";
+
+// api 호출
+function getWeather(lat, lon){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`).then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `temperature : ${temperature} @ place : ${place}`
+    });
+}
 
 
 function saveCoords(coordsObj){
@@ -15,6 +29,7 @@ function handleGeoSucces(position){
         longitude,
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -32,7 +47,8 @@ function loadCoords(){
     if(loadCoords === null){
         askForCoords();
     } else{
-
+        const parseCoords = JSON.parse(loadCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
