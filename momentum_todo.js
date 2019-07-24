9f341238074d0a4ad096d7293bbc8bd411154a36
miniyoung37 <miniyoung37@gmail.com>
2019-07-24 15:26:31 +0900

    momentum delete toDo

--- momentum_todo.js
+++ momentum_todo.js
@@ -4,9 +4,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+
 // todolist array 생성
 
 const toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo0){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -19,6 +32,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length+1;       // 나중 생성되는 li제거를 위해 순서를 주기위해
     delBtn.innerHTML = "X";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
