0a808571c476e4badc04b104840656c7408ad78b
miniyoung37 <miniyoung37@gmail.com>
2019-07-26 01:46:09 +0900

    painting step1

---
+++ painting_js/app.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("jsCanvas");
+
+let painting = false;
+
+
+function stopPainting(){
+    painting = false;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+function onMouseLeave(event){
+    stopPainting();
+}
+
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+}
