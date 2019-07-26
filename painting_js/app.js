b35745d238ab554fcf9a032ec6c57eafac72d8e8
miniyoung37 <miniyoung37@gmail.com>
2019-07-26 20:35:51 +0900

    paintingJS step4 filling click

--- painting_js/app.js
+++ painting_js/app.js
@@ -4,11 +4,18 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+
+
 
 
 let painting = false;
@@ -38,6 +45,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(evnet){
@@ -52,14 +60,23 @@ function handleModeClicke(){
     } else{
         filling = true;
         mode.innerText = "Paint";
+        ctx.fillStyle = ctx.strokeStyle;
     }
 }
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
  Array.from(colors).forEach(color => 
