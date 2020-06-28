9939ab8dd72613929c5270e4943ba152bb8c4285
LeeYoungMin <miniyoung37@gmail.com>
2020-06-28 18:07:13 +0900

    js study

--- JS_study.js
+++ JS_study.js
@@ -119,3 +119,36 @@ function getObj() {
 }
 
 let obj = getObj();
+
+/*
+Node.js 에서 예외 처리하기
+1. 비동기(async) 모듈 또는 함수의 callback에서 첫번째 매개변수로 에러 정보를 반환한다. 이에 따라 비오기 모듈 또는 함수를 호출할 때에는 먼저 첫 번째 매개변수인 
+에러정보를 확인해야 한다.
+2. try-catch, throw이다. 이는 자바스크립트 예외 처리와 동일하게 처리한다. 다만 주의해야 할 점은 첫번째 방법에서 활용한 비동기 패턴에 try-catch throw를 적용하는 것은
+잘못된 방법이다. 무조건 비동기 함수의 에러처리는 callback 함수를 활용해야 한다. 반대로 callback 함수로 처리하지 않는 기 외 패턴(동기-sync 패턴 등)에 대해서는 try-catch, throow를
+적용하여 에러를 처리한다. 
+*/
+
+/*
+Event Emitter
+'어떤 사건의 발생' 이라는 측면에서 이벤트라고 부르며, 발생된 이벤트에 대한 응답으로 반응하는것을 리스너(listener)라고 한다. Event Emitter는 바로 이벤트-리스너 패턴으로 구현된다.
+*/
+
+class Emitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(type, listener) {
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+  }
+
+  emit(type) {
+    if (this.events[type]) {
+      this.events[type].forEach((listener) => {
+        listener();
+      });
+    }
+  }
+}
